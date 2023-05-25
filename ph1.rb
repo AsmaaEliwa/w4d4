adfb7f6ad2271963d7e4ba036c9aa8c6273d9418
Joe Randazzo <jprandazzo@icloud.com>
2023-05-25 17:35:29 -0400

    window pt 1

--- ph1.rb
+++ ph1.rb
@@ -132,11 +132,34 @@ end
 def two_sum?(arr,target_sum)
     h = Hash.new
     arr.each_with_index do |e,i|
-        h[e] = i
+        h[e] = target_sum - e
+    end
+
+    h.each do |k,v| # 3 => 3
+        # debugger
+        return k, v unless h.except(k).key(k) == nil
     end
 end
  
+# p two_sum?([3,2,4,1],6)
+# p two_sum?([8,3,7,9,2,4,1],16)
 
+def windowed_max_range(arr,w) #w=4
+    # [1,0,2,5,4,8]
+    max_range = 0
+    (0...arr.length-w+1).each do |i| #i = 0
+        current_range = arr[i..i+w-1].max - arr[i..i+w-1].min #arr[0..i+w-1] #5
+        if current_range > max_range
+            max_range = current_range
+        end
+    end
+    p max_range
+end
+
+windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
+windowed_max_range([1, 0, 2, 5, 4, 8], 3)# == 5 # 0, 2, 5
+windowed_max_range([1, 0, 2, 5, 4, 8], 4)# == 6 # 2, 5, 4, 8
+windowed_max_range([1, 3, 2, 5, 4, 8], 5)# == 6 # 3, 2, 5, 4, 8
 
 
 
