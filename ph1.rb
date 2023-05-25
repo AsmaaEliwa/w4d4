6022aa230338bab8de19d96c8705d1fccf347ed0
Joe Randazzo <jprandazzo@icloud.com>
2023-05-25 12:17:06 -0400

    subsum phase 1

--- ph1.rb
+++ ph1.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 def my_min(arr) #o(n^2)
     (0...arr.length).each do |i1|
         return arr[i1] if (0...arr.length).all? do |i2|
@@ -6,7 +8,6 @@ def my_min(arr) #o(n^2)
     end
 
 end
-
 
 
 def my_min(arr)  #o(n)
@@ -20,4 +21,24 @@ min
 end
 
 
+#LARGEST CONTIGUOUS SUB SUM
+def subsum(arr) #O(n^2) because nested loop is dominant
+    arr2 = []
+    (0...arr.length).each do |i|
+        (0...arr.length).each do |j|
+            if j<i
+                arr2<<[arr[i],arr[j]]
+            end
+        end
+    end
+    
+    arr2.map { |subarr| subarr[0] + subarr[1]}.max
+end
+
+#PHASE II
+
+def subsum2(arr)
+    largest_sum = xxxx
+    
+end
 
